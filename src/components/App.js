24670671d18ddc3e3759143916d3ab2baad16a0d
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-10-17 17:11:03 -0300

    Add css frameworks
    And basic layout components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./layout/Header";
-import { Button } from "reactstrap";
+import Dashboard from "./dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header
-          appName={this.props.appName}
-          currentUser={this.props.currentUser}
-        />
-        <Button color="danger">Danger!</Button>
-        <header className="App-header" />
+      <div className="App-header">
+        <Header appName="Share-Vega" currentUser={this.props.currentUser} />
+        <Dashboard />
       </div>
     );
   }
