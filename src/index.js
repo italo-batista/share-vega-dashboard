1c33dc4c57385c3fa0d898a1f26ec85781312f11
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-10-31 00:36:52 -0300

    Add depdendencies

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
+import $ from "jquery";
+import Popper from "popper.js";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
