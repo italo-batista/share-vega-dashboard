6e1d96b950d17117ac12af49076110ecfa8ff349
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-11-20 00:45:18 -0300

    Fix header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,24 +1,9 @@
 import React from "react";
-
-const LoggedOutView = props => {
-  // if (!props.currentUser) { return ...}
-  return null;
-};
-
-const LoggedInView = props => {
-  // if (!props.currentUser) { return ...}
-  return null;
-};
+import "./Header.css";
 
 class Header extends React.Component {
   render() {
-    return (
-      <div className="container">
-        {this.props.appName}
-        <LoggedOutView currentUser={this.props.currentUser} />
-        <LoggedInView currentUser={this.props.currentUser} />
-      </div>
-    );
+    return <div className="App-header">{this.props.appName}</div>;
   }
 }
 
