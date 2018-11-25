7794e3e5133a13da4b4c8e53ea5451c7eb100c1a
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-11-25 03:34:15 -0300

    Create SortBy component

---
+++ src/components/frontelements/SortBy.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Dropdown } from "semantic-ui-react";
+
+class SortBy extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const options = this.props.options.map(optName => (
+      <Dropdown.Item key={optName} onClick={() => this.props.onClick(optName)}>
+        {optName}
+      </Dropdown.Item>
+    ));
+
+    return (
+      <Dropdown
+        text="Sort By"
+        icon="ordered list"
+        className="icon"
+        floating
+        labeled
+        button
+      >
+        <Dropdown.Menu>{options}</Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+}
+
+export default SortBy;
