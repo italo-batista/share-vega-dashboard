78961700497ab16dea16cea4161c0466643990ba
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-11-28 00:17:13 -0300

    Change Header to go up Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./layout/Header";
 import Navbar from "./layout/Navbar";
 import Dashboard from "./dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar currentUser={this.props.currentUser} />
         <div className="container">
-          <Header appName="Share-Vega" />
           <Dashboard />
         </div>
       </div>
