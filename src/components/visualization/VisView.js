d375d00bd9f09d061e03c801430de54de7b5715b
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-12-12 23:35:04 -0300

    Init VisView Component

---
+++ src/components/visualization/VisView.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class VisView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  render() {
+    return <div />;
+  }
+}
+
+export default VisView;
